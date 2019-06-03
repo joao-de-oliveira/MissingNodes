7314dee2027ef45fa292da8dcf75d8f368be9656
joao-de-oliveira <joaodeoliveira@ua.pt>
2019-06-03 09:36:07 +0100

    main changes

--- MissingNodes/main.py
+++ MissingNodes/main.py
@@ -8,30 +8,27 @@ np.set_printoptions(threshold=np.inf)
 #Input
 connected = True
 repetitions = 50
-#filename = 'netscience'
+filename = 'dolphins'
 labeltype='labels'
-MaxNodesRemoved=160
+MaxNodesRemoved=150
 AffinityType='AA'
+DensityTolerance=0.005
 #File Parsing
+G = nx.read_gml(filename+'.gml',label=labeltype)
+G = parse_G(G,labeltype,connected)
+ForbiddenList = create_forbidden_list(G)
 #Aux Code
-for filename in ['dolphins','lesmis','adjnoun','polbooks']:
-    G = nx.read_gml(filename+'.gml',label=labeltype)
-    G = parse_G(G,labeltype,connected)
-    ForbiddenList = create_forbidden_list(G)
-    for DensityTolerance in frange(0.005,0.50,0.005):
-        F=1
-        AUC=np.zeros(repetitions,dtype=float)
-        Ratio=np.zeros(repetitions,dtype=float)
-        for i in range(0, repetitions):
-            #Network Object Creation & Parsing
-            A = Network(G,connected=connected,node_removal_discrete=3,ForbiddenList=ForbiddenList,DensityTolerance=DensityTolerance)
-            if i == 0:
-                Nodes_Removed = A.nodes_to_predict
-            try:
-                Gtest = Network.create_test_network(A)
-            except:
-                F=0
-                break
+for node_removal_discrete in range(1,MaxNodesRemoved):
+    F=1
+    AUC=np.zeros(repetitions,dtype=float)
+    Ratio=np.zeros(repetitions,dtype=float)
+    for i in range(0, repetitions):
+        #Network Object Creation & Parsing
+        A = Network(G,connected=connected,node_removal_discrete=node_removal_discrete,ForbiddenList=ForbiddenList,DensityTolerance=DensityTolerance)
+        if i == 0:
+            Nodes_Removed = A.nodes_to_predict
+        try:
+            Gtest = Network.create_test_network(A)
             #AUC
             Network.create_boolmatrix(A)
             Gphantom = Network.create_phantoms(A,Gtest)
@@ -45,9 +42,11 @@ for filename in ['dolphins','lesmis','adjnoun','polbooks']:
             Ratio[i] = GED/A.TestGraphEditDistance
             print('Ratio = ', Ratio[i])
             del A, Gtest
-        if F > 0:
-            PrintTolerance=DensityTolerance*100
-            np.savetxt(filename+'_'+'AA'+'_AUC'+'_'+str(PrintTolerance)+'.csv',AUC,delimiter=',')
-            np.savetxt(filename+'_'+'AA'+'_Ratio'+'_'+str(PrintTolerance)+'.csv',Ratio,delimiter=',')
-        else:
-            pass
+        except:
+            F=0
+            break
+    if F > 0:
+        np.savetxt(filename+'_'+'AA'+'_AUC'+'_'+str(node_removal_discrete)+'.csv',AUC,delimiter=',')
+        np.savetxt(filename+'_'+'AA'+'_Ratio'+'_'+str(node_removal_discrete)+'.csv',Ratio,delimiter=',')
+    else:
+        pass
